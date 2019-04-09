4cc1188c2c98ca31ddb40967b2e09d8a3d11f68d
dong <gengyudonga@gmail.com>
2019-04-09 19:20:58 +0800

    clean

--- mock/index.js
+++ mock/index.js
@@ -2,13 +2,11 @@ import Mock from 'mockjs'
 import { param2Obj } from '../src/utils'
 
 import user from './user'
-// import role from './role'
 import article from './article'
 import search from './remoteSearch'
 
 const mocks = [
   ...user,
-  // ...role,
   ...article,
   ...search
 ]
